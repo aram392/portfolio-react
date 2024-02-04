5fa444cbaf27f1185206539360e95602e669f03f
Aram Balayan <43249566+aram392@users.noreply.github.com>
2024-02-03 17:00:24 -0800

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 import {
   Flex,
